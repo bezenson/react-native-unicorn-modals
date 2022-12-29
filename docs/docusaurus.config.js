a4bc87ced08b0685da85fb6007d4241183e8c888
Vladislav Bezenson <vladvvb@gmail.com>
2022-12-29 11:45:28 +0300

    docs: default dark mode, remove unused images

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -51,6 +51,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+      },
       navbar: {
         title: 'React Native Unicorn Modals',
         logo: {
